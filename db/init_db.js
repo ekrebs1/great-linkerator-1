31ab7d90a094569382c6fe18ddefda0354b9c2cb
Michelle <mchllgndrn@gmail.com>
2021-06-08 21:11:40 -0500

    MG/chore/initial setup

--- db/init_db.js
+++ db/init_db.js
@@ -7,15 +7,35 @@ const {
 async function buildTables() {
   try {
     client.connect();
+        // drop tables in correct order
+        client.query(`
+        DROP TABLE IF EXISTS tags;
+        DROP TABLE IF EXISTS links;
+      `);
 
-    // drop tables in correct order
+        // build tables in correct order
+        console.log("Starting to build tables...");
 
-    // build tables in correct order
+        await client.query(`
+          CREATE TABLE links(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) UNIQUE NOT NULL,
+            link VARCHAR(255) UNIQUE NOT NULL,
+            "createDate" DATE NOT NULL,
+            "clickNum" INTEGER,
+            comment VARCHAR(255) NOT NULL
+          );
+           CREATE TABLE tags(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) UNIQUE NOT NULL
+          );
+          `);
+        console.log("Finished building tables...");
+      } catch (error) {
+        throw error;
+      }
+    }
 
-  } catch (error) {
-    throw error;
-  }
-}
 
 async function populateInitialData() {
   try {
