e5199c493a6bf50d16f6843b2f640365a8add064
elijahwitt <elijahwitthensel@gmail.com>
2021-06-09 09:06:58 -0500

    EW: npm run db:build creates tables for link test data

--- db/init_db.js
+++ db/init_db.js
@@ -3,12 +3,13 @@ const {
   client,
   // other db methods
 } = require("./index");
+const {createLinks} =  require("./links");
 
 async function buildTables() {
   try {
     client.connect();
-        // drop tables in correct order
-        client.query(`
+    // drop tables in correct order
+    client.query(`
         DROP TABLE IF EXISTS tags;
         DROP TABLE IF EXISTS links;
       `);
@@ -26,7 +27,7 @@ async function buildTables() {
       CREATE TABLE links(
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) UNIQUE NOT NULL,
-        link VARCHAR(255) UNIQUE NOT NULL,
+        link VARCHAR(255) NOT NULL,
         "createDate" DATE NOT NULL,
         "clickNum" INTEGER,
         comment VARCHAR(255) NOT NULL
@@ -44,53 +45,44 @@ async function buildTables() {
 }
 
 const createInitialLinks = async () => {
-  console.log("Starting to create initial links...")
+  console.log("Starting to create initial links...");
   try {
     const linksToCreate = [
-      { name: "Google", link: "https://www.google.com", createDate: "2020/08/31", clickNum: 12, comment:"A search utility tool."},
-      { name: "Facebook", link: "https://www.facebook.com", createDate: "2021/02/14", clickNum: 23, comment:"A social media website for family and friends."},
-      { name: "LinkedIn", link: "https://www.linkedin.com", createDate: "2020/12/25", clickNum: 1, comment:"A social networking site for professional relationships"},
+      {
+        name: "Google",
+        link: "https://www.google.com",
+        createDate: "2020/08/31",
+        clickNum: 12,
+        comment: "A search utility tool.",
+      },
+      {
+        name: "Facebook",
+        link: "https://www.facebook.com",
+        createDate: "2021/02/14",
+        clickNum: 23,
+        comment: "A social media website for family and friends.",
+      },
+      {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com",
+        createDate: "2020/12/25",
+        clickNum: 1,
+        comment: "A social networking site for professional relationships",
+      },
     ];
+    const links = await Promise.all(linksToCreate.map(createLinks));
+    console.log("Links created:");
+    console.log(links);
+    console.log("Finished creating links!");
   } catch (err) {
-    console.error("There was a problem creating LINKS")
-    throw err
+    console.error("There was a problem creating LINKS");
+    throw err;
   }
-}
+};
 
 async function populateInitialData() {
   try {
-    // create useful starting data
-    const createInitialLinks = async () => {
-      console.log("Starting to create initial links...");
-      try {
-        const linksToCreate = [
-          {
-            name: "Google",
-            link: "https://www.google.com",
-            createDate: "2020/08/31",
-            clickNum: 12,
-            comment: "A search utility tool.",
-          },
-          {
-            name: "Facebook",
-            link: "https://www.facebook.com",
-            createDate: "2021/02/14",
-            clickNum: 23,
-            comment: "A social media website for family and friends.",
-          },
-          {
-            name: "LinkedIn",
-            link: "https://www.linkedin.com",
-            createDate: "2020/12/25",
-            clickNum: 1,
-            comment: "A social networking site for professional relationships",
-          },
-        ];
-      } catch (err) {
-        console.error("There was a problem creating LINKS");
-        throw err;
-      }
-    };
+    await createInitialLinks()
   } catch (error) {
     throw error;
   }
