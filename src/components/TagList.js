95b383bdfad562375a948224297c26389c5615a3
elijahwitt <elijahwitthensel@gmail.com>
2021-06-13 21:38:03 -0500

    broken tag list component

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,46 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { Chip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { getLinkTags } from '../api';
+import { getLinkTags } from "../api";
 
-const TagList = ({link}) => {
-    const [targetLink, setTargetLink] = useState(link)
-    const [targetTag, setTargetTag] = useState([])
-    const [linkTags, setLinkTags] = useState([])
+const TagList = ({ link }) => {
+  const [targetLink, setTargetLink] = useState(link);
+  // const [targetTag, setTargetTag] = useState([])
+  const [linkTags, setLinkTags] = useState();
 
-    // useEffect(async () => {
-    //     const tags = await getLinkTags()
-    //     setLinkTags(tags)
-    // }, [])
+  useEffect( () => {
+    getLinkTags()
+    .then((response) => {
+      setLinkTags(response);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, [setLinkTags])
 
-    console.log(linkTags, "LINK TAGS")
-    console.log(targetLink, "TARGET LINK")
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          display: "flex",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          listStyle: "none",
-          padding: theme.spacing(0.5),
-          margin: 0,
-        },
-        chip: {
-          margin: theme.spacing(0.5),
-        },
-      }));
-    
-      const classes = useStyles();
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      flexWrap: "wrap",
+      listStyle: "none",
+      padding: theme.spacing(0.5),
+      margin: 0,
+    },
+    chip: {
+      margin: theme.spacing(0.5),
+    },
+  }));
 
+  const classes = useStyles();
 
- return link.tags.map((tag) => {
-    if (link.id === tag.id) {
-        return (
-     <li key={tag.id}>
-       <Chip label={tag.name} className={classes.chip} />
-     </li>
-   );
+  return link.tags.map((tag) => {
+      linkTags.map((linkTag) => {
+          if (linkTag.tagId === tag.id) {
+              return (
+                <li key={tag.id}>
+                  <Chip label={tag.name} className={classes.chip} />
+                </li>
+              );
+          }
+      })
     }
-   
- });
-}
-export default TagList
+  );
+};
+export default TagList;
