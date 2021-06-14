252dea52329bfd8796f4c794e203b70a2ba15567
Erica <ericakrebs1@gmail.com>
2021-06-14 18:05:18 -0500

    Started  App layout/title

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-  import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#15616d",
+    },
+    secondary: {
+      main: "#ac85f9",
+    },
+  },
+});
+
+export default theme;
